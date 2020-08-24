87dc9fd31a5401621606e16ca2dfe66f5773cae0
Clayton Pierce <bamer78@gmail.com>
2020-08-24 22:54:49 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,49 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+end
+def won?(board)
+  WIN_COMBINATIONS.each do |pos1, pos2, pos3|
+    moves = [board[pos1],board[pos2],board[pos3]]
+    ((moves == ["X","X","X"]) || (moves == ["O","O","O"])) ? (return [pos1, pos2, pos3]) : nil
+  end
+  return false
+end
+def full?(board)
+  ((board.count("X") + board.count("O")) == 9) ? true:false
+end
+def draw?(board)
+  (full?(board) && !(won?(board)))? true:false
+end
+def over?(board)
+  (won?(board) || draw?(board) || full?(board))? true:false
+end
+def winner(board)
+  (won?(board)) ? (return board[won?(board)[0]]) : nil
+end
+
+  
