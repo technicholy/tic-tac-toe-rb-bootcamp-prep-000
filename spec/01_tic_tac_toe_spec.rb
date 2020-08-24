723602984a18f7b049cc97250a967a89838c61ee
Clayton Pierce <bamer78@gmail.com>
2020-08-24 23:45:04 +0000

    Automatically backed up by Learn

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -259,13 +259,13 @@ describe './lib/tic_tac_toe.rb' do
 end
 def play(board)
   turn(board)
-  # if over?(board)
-  #   if won?(board)
-  #     puts `The winner is #{(board[won?(board)[0]] == "X") ? "X":"O"}`
-  #   else
-  #     puts "Cat's Game!"
-  #   end
-  # else
-  #   play(board)
-  # end
+  if over?(board)
+    if won?(board)
+      puts `The winner is #{(board[won?(board)[0]] == "X") ? "X":"O"}`
+    else
+      puts "Cat's Game!"
+    end
+  else
+    play(board)
+  end
 end
